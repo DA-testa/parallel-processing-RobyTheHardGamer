cf65b70d4236891bfff8260651ea259d0131744c
Roberts Kristofers Strautiņš <124698215+RobyTheHardGamer@users.noreply.github.com>
2023-04-25 12:54:15 +0000

    test19

--- main.py
+++ main.py
@@ -4,19 +4,15 @@ n, m = map(int, input().split())
 processing_times = list(map(int, input().split()))
 
 # Create a priority queue of threads and their completion times, initialized with 0 completion time
-pq = [(0, i) for i in range(n)]
+pq = [(0, i) for i in range(n)] #izveidos prioritātes rindu ar pavedieniem un to izpildes laikus 
 heapq.heapify(pq)
 
 for i in range(m):
-    # Pop the thread with the smallest completion time from the priority queue
-    completion_time, thread_idx = heapq.heappop(pq)
+    completion_time, thread_idx = heapq.heappop(pq) #izņemt padevienu ar mazāko izpildas laiku no prioritātes rindas
 
-    # Assign the thread to the current job
-    start_time = completion_time
+    start_time = completion_time #uzdot pavedienam tagatējo uzdevumu
     end_time = start_time + processing_times[i]
 
-    # Output the assigned thread and start time of the current job
-    print(thread_idx, start_time)
+    print(thread_idx, start_time) #attēlot piesšķirto pavedienu un sākuma laiku tagatējam uzdevumam
 
-    # Update the completion time of the assigned thread and add it back to the priority queue
-    heapq.heappush(pq, (end_time, thread_idx))
+    heapq.heappush(pq, (end_time, thread_idx)) #attēlo uzdevuma izpildes laiku no katra pavediena un pievienot to atpakaļ prioritātes rindai
